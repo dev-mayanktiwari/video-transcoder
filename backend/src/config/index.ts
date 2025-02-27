b1344e4bb02eac17529f9f0d99308167e56c4424
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-27 17:33:30 +0530

    feat: Updated config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,13 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-type ConfigKeys = "PORT" | "DATABASE_URL" | "SERVER_URL" | "ENV";
+type ConfigKeys =
+  | "PORT"
+  | "DATABASE_URL"
+  | "SERVER_URL"
+  | "ENV"
+  | "BUCKET_REGION"
+  | "AMAZON_ACCESS_KEY"
+  | "AMAZON_SECRET_ACCESS_KEY"
+  | "BUCKET_NAME_NORMAL_UPLOAD"
+  | "BUCKET_NAME_HLS_UPLOAD";
 
 const _config: Record<ConfigKeys, string | undefined> = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   SERVER_URL: process.env.SERVER_URL,
   ENV: process.env.ENV,
+  BUCKET_REGION: process.env.BUCKET_REGION,
+  AMAZON_ACCESS_KEY: process.env.AMAZON_ACCESS_KEY,
+  AMAZON_SECRET_ACCESS_KEY: process.env.AMAZON_SECRET_ACCESS_KEY,
+  BUCKET_NAME_NORMAL_UPLOAD: process.env.BUCKET_NAME_NORMAL_UPLOAD,
+  BUCKET_NAME_HLS_UPLOAD: process.env.BUCKET_NAME_HLS_UPLOAD,
 };
 
 export const AppConfig = {
