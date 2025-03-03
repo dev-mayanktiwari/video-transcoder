da670e11777de11b7309908e700c6c5e97fbec50
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-03 21:07:53 +0530

    feat: Added conditional baseUrl

--- frontend/src/utils/endpoints.ts
+++ frontend/src/utils/endpoints.ts
@@ -1,4 +1,8 @@
-const baseUrl = process.env.NEXT_PUBLIC_LOCAL_BACKEND_BASE_URL;
+const env = process.env.NEXT_PUBLIC_ENV;
+const baseUrl =
+  env === "development"
+    ? process.env.NEXT_PUBLIC_LOCAL_BACKEND_BASE_URL
+    : process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_BASE_URL;
 
 export const ENDPOINTS = {
   GET_PRESIGNED_URL: `${baseUrl}/upload/getPresignedUrl`,
